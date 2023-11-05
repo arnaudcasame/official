a55b03aeeef7a2c0337039247cf2b6e7e0f0cb1f
tizekod <noon.tizekod@gmail.com>
2023-11-05 17:47:07 -0500

    Adds image assets parsing to webpack configuration

--- webpack.prod.js
+++ webpack.prod.js
@@ -27,7 +27,11 @@ module.exports = {
                     loader: "sass-loader",
                     options: {},
                 }],
-            }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
         ],
     },
 };
