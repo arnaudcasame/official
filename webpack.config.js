b81e3116aaa2648d66272a4d4d6433c341e37c3c
tizekod <noon.tizekod@gmail.com>
2023-11-05 00:48:24 -0400

    Configures Webpack to support SASS/SCSS rules

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'builds/production/js'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    {
+                        loader: "sass-loader",
+                        options: {},
+                    }
+                ],
+            },
+        ],
     },
 };
