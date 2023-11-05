e717f44b27ec163f330af320d6b35438776e0627
tizekod <noon.tizekod@gmail.com>
2023-11-05 15:12:21 -0500

    Alllows webpack to load images resources

--- webpack.dev.js
+++ webpack.dev.js
@@ -28,7 +28,11 @@ module.exports = {
                     loader: "sass-loader",
                     options: {},
                 }],
-            }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
         ],
     },
 };
