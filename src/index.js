6bd8771cdad73c818e47dbc20cc5db01169ab1be
tizekod <noon.tizekod@gmail.com>
2023-11-05 01:19:35 -0400

    Adds HTML dynamically to the index page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './process/sass/style.scss';
+
+const presentation = document.createElement('section');
+presentation.className = 'levels show';
+presentation.id = 'overall';
+
+document.body.append(presentation);
