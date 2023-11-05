f28e99dba23ab925696a6167131f6981347b2235
tizekod <noon.tizekod@gmail.com>
2023-11-05 02:09:29 -0500

    Removes unnecessary JS code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
 import './process/sass/style.scss';
 
-const presentation = document.createElement('section');
-presentation.className = 'levels show';
-presentation.id = 'overall';
-
-document.body.append(presentation);
