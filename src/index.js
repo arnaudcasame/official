1e24823f87df879972f08d13460ad874db50f3ad
tizekod <noon.tizekod@gmail.com>
2023-11-14 16:42:04 -0500

    Import skills script to the main JS module

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './process/sass/style.scss';
 
 import me from './process/sass/images/me.jpg';
+import {  } from "./process/js/myscript";
 
 const mainHero = document.querySelector('#main-hero');
 
