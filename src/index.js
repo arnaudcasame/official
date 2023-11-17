51757922d530fb46887a060eb45f0833c9d61752
tizekod <noon.tizekod@gmail.com>
2023-11-17 03:21:29 -0500

    Imports & displays WorkCards

--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@ import './process/sass/style.scss';
 
 import me from './process/sass/images/me.jpg';
 import { CardBoard } from "./process/js/myscript";
-import {  } from "./process/js/work-info";
+import { WorkCard } from "./process/js/work-info";
+
+// Adds my picture to the first page
+const mainHero = document.querySelector('#main-hero');
+mainHero.src = me;
 
 
 
-const dataUrl = 'https://gist.githubusercontent.com/arnaudcasame/18db83b6e6791c728bd992f70f1b7d4d/raw/dc4a6ee6b1f107efaa08e132f9c73a0a7d784d26/portfolio.json'
+const dataUrl = 'https://gist.githubusercontent.com/arnaudcasame/18db83b6e6791c728bd992f70f1b7d4d/raw/3fb194126d259dbafd7721fc9344623f288b459b/portfolio.json'
 fetch(dataUrl)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
+
+        for (const project of data.projects) {
+            new WorkCard('projects', project)
+        }
+
         const skills = document.querySelector('.skills-container');
         const board = new CardBoard(skills, data.tools);
         skills.addEventListener('click', function (e) {
@@ -31,8 +40,4 @@ fetch(dataUrl)
     })
     .catch(function (error) {
         console.warn(error)
-    })
-
-const mainHero = document.querySelector('#main-hero');
-
-mainHero.src = me;
+    });
