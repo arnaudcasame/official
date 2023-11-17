75fe98c63458cd0aa1735f01fe37124f7638069f
tizekod <noon.tizekod@gmail.com>
2023-11-16 22:16:08 -0500

    Creates & imports work info JS module

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './process/sass/style.scss';
 
 import me from './process/sass/images/me.jpg';
 import {  } from "./process/js/myscript";
+import {  } from "./process/js/work-info";
 
 const mainHero = document.querySelector('#main-hero');
 
