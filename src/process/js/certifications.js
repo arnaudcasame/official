c3fbfc2180171f479e78ffc472c1c2918905ec29
Tizekod <noon.tizekod@gmail.com>
2023-12-09 01:32:43 -0500

    Implements certification modal

--- src/process/js/certifications.js
+++ src/process/js/certifications.js
@@ -17,9 +17,53 @@ export class Certification {
         
         this.timeStamp = Date.parse(data.date);
 
-        
-
         certs.appendChild(cert);
+
+        cert.addEventListener('click', function(e){
+
+            if(!navigator.userAgent.includes('iPhone') && !navigator.userAgent.includes('Android')){
+                const modalContainer = document.createElement('div');
+                modalContainer.setAttribute('class', 'modal-container');
+    
+                certs.parentElement.appendChild(modalContainer);
+    
+                const frame = document.createElement('div');
+                frame.setAttribute('class', 'modal-frame');
+    
+                const header = document.createElement('div');
+                header.setAttribute('class', 'frame-header');
+                
+                const title = document.createElement('h3');
+                title.textContent = data.school;
+                header.appendChild(title);
+    
+                const closeIcon = document.createElement('i');
+                closeIcon.setAttribute('class', 'fa fa-window-close close-btn');
+                header.appendChild(closeIcon);
+    
+    
+                frame.appendChild(header);
+                
+                const body = document.createElement('div');
+                body.setAttribute('class', 'frame-body');
+                frame.appendChild(body);
+    
+                
+                const image = document.createElement('img');
+                image.setAttribute('class', 'cert-img');
+                image.setAttribute('src', data.url);
+    
+                const imgContainer = document.createElement('div');
+                imgContainer.appendChild(image);
+                body.appendChild(imgContainer)
+    
+                modalContainer.appendChild(frame);
+    
+                closeIcon.addEventListener('click', function(){
+                    certs.parentElement.removeChild(modalContainer);
+                });
+            }
+        });
     }
 
     getTimeStamp(){
