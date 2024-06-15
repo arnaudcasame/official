0ad7199c3f08328985ebfb474fece2b3eef8168c
Tizekod <noon.tizekod@gmail.com>
2024-06-14 21:41:40 -0400

    Updates webpack prod build config

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'builds/arnaudcasame.github.io'),
+        path: path.resolve(__dirname, 'builds/production'),
         clean: true
     },
     plugins: [
