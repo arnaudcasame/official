f12b13d8ffdd4deda29d75570cb4bed8b700de54
Tizekod <noon.tizekod@gmail.com>
2024-06-27 21:51:42 -0400

    Implements the window explorer drag and drop feature

--- src/process/js/explorer.js
+++ src/process/js/explorer.js
@@ -4,6 +4,16 @@ export class Explorer {
         this.explorerEl = document.querySelector('#window-explorer');
         this.explorerEl.classList.add('open');
         console.dir(this.explorerEl);
+
+        this.windowMoveHandler = this.onWindowMove.bind(this);
+        this.mouseDownHandler = this.onMouseDown.bind(this);
+        this.mouseUpHandler = this.onMouseUp.bind(this);
+
+        this.header = this.explorerEl.children[0];
+        this.startX = 0;
+        this.startY = 0;
+        this.header.addEventListener('mousedown', this.mouseDownHandler);
+        this.header.addEventListener('touchstart', this.mouseDownHandler);
     }
 
     showExplorer() {
@@ -16,5 +26,32 @@ export class Explorer {
 
     toggleExplorer() {
         this.explorerEl.classList.toggle('open');
+    }
+
+    onWindowMove(event){
+        const newX = this.startX - event.clientX;
+        const newY = this.startY - event.clientY;
+
+        this.startX = event.clientX;
+        this.startY = event.clientY;
+
+        this.explorerEl.style.top = (this.explorerEl.offsetTop - newY) + 'px';
+        this.explorerEl.style.left = (this.explorerEl.offsetLeft - newX) + 'px';
+    }
+
+    onMouseDown(event){
+        this.startX = event.clientX;
+        this.startY = event.clientY;
+
+        document.addEventListener('mousemove', this.windowMoveHandler);
+        document.addEventListener('touchmove', this.windowMoveHandler);
+
+        document.addEventListener('mouseup', this.mouseUpHandler);
+        document.addEventListener('touchend', this.mouseUpHandler);
+    }
+
+    onMouseUp(event){
+        document.removeEventListener('mousemove', this.windowMoveHandler);
+        document.removeEventListener('touchmove', this.windowMoveHandler);
     }
 }
